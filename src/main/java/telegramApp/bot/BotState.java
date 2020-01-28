51af30c1d098b7d7ba9aa823c4bc5d6d885abacb
Shell26 <M16VSAK47vsgit>
2020-01-28 18:25:17 +0400

    qwer

--- src/main/java/telegramApp/bot/BotState.java
+++ src/main/java/telegramApp/bot/BotState.java
@@ -13,7 +13,6 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.Keyboard
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiValidationException;
-import telegramApp.dto.CompanyDto;
 import telegramApp.dto.LocationDto;
 import telegramApp.dto.SongResponse;
 import telegramApp.model.TelegramMessage;
