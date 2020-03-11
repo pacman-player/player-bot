23bbcb9278d8b76fc449475c428faf1f8895b9bb
elrostov <qepgiq-sovcan-6roqWy>
2020-03-11 16:33:42 +0300

    Edited table name.

--- src/main/java/telegramApp/model/TelegramMessage.java
+++ src/main/java/telegramApp/model/TelegramMessage.java
@@ -3,8 +3,10 @@ package telegramApp.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "telegram_messages")
 public class TelegramMessage {
 
     @Id
