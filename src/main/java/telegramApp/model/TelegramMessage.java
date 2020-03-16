eae484457077805a70e9186afee9026d327e0a3d
elrostov <qepgiq-sovcan-6roqWy>
2020-03-16 17:35:28 +0300

    Диалект Postgres изменен на более новый - 9.4. Удален класс для MySQL.

--- src/main/java/telegramApp/model/TelegramMessage.java
+++ src/main/java/telegramApp/model/TelegramMessage.java
@@ -1,9 +1,7 @@
 package telegramApp.model;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "telegram_messages")
